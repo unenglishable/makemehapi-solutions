37b4584e931cfa07c2300dee28206570d4779dd0
unenglishable <unenglishable@gmail.com>
2014-10-09 14:47:24 -1000

    solution for 3

--- handlers.js
+++ handlers.js
@@ -1,9 +1,13 @@
+var path = require('path');
 handlers = {};
 handlers.helloHapi = function(request, reply) {
   reply("Hello Hapi");
 };
 handlers.routeReply = function(request, reply) {
   reply('Hello ' + request.params.name);
+};
+handlers.handling = {
+  file: path.join(__dirname, '3_handling', 'index.html')
 };
 
 module.exports = handlers;
